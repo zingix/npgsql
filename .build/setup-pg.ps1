671ea570075fca37efc2aa885ceeaf24b80b74f4
Shay Rojansky <roji@roji.org>
2017-03-15 00:48:32 +0200

    Further

--- .build/setup-pg.ps1
+++ .build/setup-pg.ps1
@@ -4,10 +4,11 @@ Add-Content 'C:\Program Files\PostgreSQL\9.5\data\postgresql.conf' "`nmax_prepar
 
 Write-Host Enabling PostgreSQL SSL...
 Add-Content 'C:\Program Files\PostgreSQL\9.5\data\postgresql.conf' "`nssl = true"
+Copy-Item .build\server.* "C:\Program Files\PostgreSQL\9.5\data"
 
+Write-Host Enabling PostGIS...
 If (!(Test-Path $env:POSTGIS_EXE)) {
   Write-Host Downloading PostGIS...
   (New-Object Net.WebClient).DownloadFile("http://download.osgeo.org/postgis/windows/pg95/$env:POSTGIS_EXE", "$env:POSTGIS_EXE")
 }
-
 iex ".\$env:POSTGIS_EXE /S /D='C:\Program Files\PostgreSQL\9.5'"
